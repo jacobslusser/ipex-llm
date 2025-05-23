d38b7196f75e383aaa0a27c02d9071852664ff63
Jacob Slusser <jslusser.mobile@gmail.com>
2025-05-23 14:52:50 -0700

    Configures Docker to run Ollama with model automatically

--- docker/llm/inference-cpp/start-ollama.sh
+++ docker/llm/inference-cpp/start-ollama.sh
@@ -1,3 +1,15 @@
+# print some gpu info
+echo "Mapped GPU info (sycl-ls):"
+sycl-ls
+echo ""
+
+# set timezone
+if [ -n "$TZ" ]; then
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+    echo $TZ > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata > /dev/null 2>&1 || true
+fi
+
 # init ollama first
 mkdir -p /llm/ollama
 cd /llm/ollama
@@ -5,5 +17,10 @@ init-ollama
 export OLLAMA_NUM_GPU=999
 export ZES_ENABLE_SYSMAN=1
 
+# import the model
+echo "FROM $OLLAMA_MODELPATH" > Modelfile
+./ollama create $OLLAMA_MODELNAME -f Modelfile
+
 # start ollama service
-(./ollama serve > ollama.log) &
+echo "Starting Ollama server..."
+exec ./ollama serve
